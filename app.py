b268a15cf5e1cc4df2d4d680cd58b4879b0e8654
juice <jmcclusky17@gmail.com>
2018-08-07 18:50:09 -0400

    Linux LocalHost: -Changed SQLALCHEMY_DATABASE_URI to match database and mysql port.
    -In app.py set ['DEBUG'] to False so that SSLify Https redirect works.

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 sslify = SSLify(app)
 
-app.config['DEBUG'] = True
+# TODO - Change DEBUG to False in order for the flask-sslify extenion to work. Redirects incoming requests to Https.
+# obviously, change to True to see DEBUG in Terminal when running app.
+app.config['DEBUG'] = False
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:12345@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:Blogz@localhost:3306/Blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'PublicTestKey' # TODO - Made Public! Change this for private app! :)
