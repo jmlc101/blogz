5427d07ef17666149dd851b00ff2dbb5452b1d78
juice <jmcclusky17@gmail.com>
2018-09-13 22:04:44 -0400

    Heroku Buildpack Config: -added requirements.txt, as Heroku's Python Buildpack suggests.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sslify = SSLify(app)
 
 # TODO - Change DEBUG to False in order for the flask-sslify extenion to work. Redirects incoming requests to Https.
 # obviously, change to True to see DEBUG in Terminal when running app.
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:Blogz@localhost:3306/Blogz'
